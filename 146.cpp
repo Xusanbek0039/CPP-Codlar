74b147825bab1db5502fc790c597a59bd609cfeb
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2024-01-17 01:27:11 +0500

    a

---
+++ 146.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    double x = 3.251;
+    double y = 0.325;
+    double z = 0.466e-4;
+
+    std::cout << "x = " << x << std::endl;
+    std::cout << "y = " << y << std::endl;
+    std::cout << "z = " << z << std::endl;
+
+    return 0;
+}
---
+++ 147.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    double x = 17.421;
+    double y = 10.365e-3;
+    double z = 0.828e5;
+
+    std::cout << "x = " << x << std::endl;
+    std::cout << "y = " << y << std::endl;
+    std::cout << "z = " << z << std::endl;
+
+    return 0;
+}
