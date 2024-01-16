e463fc1534f90fa614e6ef6622a3506e51360873
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2024-01-17 01:28:38 +0500

    Create 148.cpp

---
+++ 148.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    double x = 12.3e-1;
+    double y = 15.4;
+    double z = 0.252e3;
+
+    std::cout << "x = " << x << std::endl;
+    std::cout << "y = " << y << std::endl;
+    std::cout << "z = " << z << std::endl;
+
+    return 0;
+}
