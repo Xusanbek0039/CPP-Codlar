a09fd4d4b030350d7e37ffe0d9d2793c3071aa20
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2024-01-17 01:33:31 +0500

    Create 151.cpp

---
+++ 151.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int x = 1;
+    int y = 1;
+    int z = 3;
+
+    std::cout << "x = " << x << std::endl;
+    std::cout << "y = " << y << std::endl;
+    std::cout << "z = " << z << std::endl;
+
+    return 0;
+}
